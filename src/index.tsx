0ce5e29b32bc727281c1ec6dee3821ce5d85581b
Vladimir Musikhin <hey@vipho.cc>
2022-11-10 14:30:49 +0400

    Changed structure; Added router, store

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import { Provider } from 'react-redux'
+import { store } from './store/store'
+import reportWebVitals from './reportWebVitals'
+
+import Main from './views/Main/Main'
+
+import './index.css'
+import React from 'react'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+)
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+  },
+  {
+    path: '*',
+    element: <Main />,
+  },
+])
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </ React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
